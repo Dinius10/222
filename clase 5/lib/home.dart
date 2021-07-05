8a9302ce89bea3a6edd031375f73109c85fedbb9
Dinius10 <you@example.com>
2021-07-04 21:49:15 -0400

    clase 5

---
+++ clase 5/lib/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'description_place.dart';
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final descriptionPlace = Container(
+      margin: EdgeInsets.only(
+          top: 250,
+          left: 30,
+          right: 30
+      ),
+      child: DescriptionPlace(),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            "My Place"
+        ),
+      ),
+    body: Stack(
+      children: <Widget>[
+        descriptionPlace
+      ],
+    ),
+    );
+  }
+
+}
